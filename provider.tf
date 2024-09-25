8fd6b948bb5fabea9a5ac84e1749d96158a441db
divya kamatham <divyakamatham12@gmai.com>
2024-09-25 16:25:42 +0800

    Add files

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "1s-remote-state"
+    key    = "expense-vpcc"
+    region = "us-east-1"
+    dynamodb_table = "1s-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
